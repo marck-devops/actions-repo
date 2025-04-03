4e780ac6d91203cd0786bb691f1b56581c53d504
marck-devops <maltidor86@gmail.com>
2025-04-03 13:13:05 -0400

    Job Well Done!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Mike with devops and github actions!"
+    return "Hello, I'm Mike with devops and github actions. I just pushed my build to GHA and that went to my ECR. From there I created a EC2 instance to pull the image so I can build the app. It was successful!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
